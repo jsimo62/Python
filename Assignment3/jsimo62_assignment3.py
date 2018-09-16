c98395daed2ec785f63ca8cd972f3a2841b33192
jsimo62 <42632473+jsimo62@users.noreply.github.com>
2018-09-16 14:15:02 -0500

    contents of file update

--- Assignment3/jsimo62_assignment3.py
+++ Assignment3/jsimo62_assignment3.py
@@ -1,1 +1,8 @@
+# Import CSV class to work with CSV files
+import csv
 
+with open('mpg.csv') as file
+    csv.reader(file)
+    f_mpg = csv.reader(file)
+    for row in f_mpg:
+        print(f_mpg)
