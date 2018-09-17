e6e9438beb17a0f591c7503f49fbaef1f2e1c122
jsimo62 <42632473+jsimo62@users.noreply.github.com>
2018-09-16 20:46:17 -0500

    updates

--- Assignment3/jsimo62_assignment3.py
+++ Assignment3/jsimo62_assignment3.py
@@ -10,6 +10,7 @@ with open('mpg.csv', newline='') as csvfile:
     # initiate sets
     s_manuf, s_class = set(),set()
 
+    ls_audi= []
     # Loop through csv
     for row in f_mpg:
         # Create a set of unique manufacurers and classes
@@ -19,29 +20,53 @@ with open('mpg.csv', newline='') as csvfile:
         ls_avg_cm_all.append(int(row['cty']))
         ls_avg_hm_all.append(int(row['hwy']))
 
+        if row['manufacturer'] == 'audi':
+            ls_audi.append(int(row['hwy']))
+        #elif row['manufacturer'] == 'chevrolet':
+        #    ls_chev.append(int(row['hwy']))
+        #elif row['manufacturer'] == 'dodge':
+        #    ls_chev.append(int(row['hwy']))
+        #elif row['manufacturer'] == 'ford':
+        #    ls_chev.append(int(row['hwy']))
+        #elif row['manufacturer'] == 'honda':
+        #    ls_chev.append(int(row['hwy']))
+        #elif row['manufacturer'] == 'hyundai':
+        #    ls_chev.append(int(row['hwy']))
+        #elif row['manufacturer'] == 'jeep':
+        #    ls_chev.append(int(row['hwy']))
+        #elif row['manufacturer'] == 'land rover':
+        #    ls_chev.append(int(row['hwy']))
+    ls_manufacturer = s_manuf
+    ls_class = s_class
+    #print(sorted(ls_manufacturer))
+    #print(sorted(ls_class))
+    #print(ls_audi)
 
     # Calculate the average for all vehicles
     calc_avg_cm_all = sum(ls_avg_cm_all)/len(ls_avg_cm_all)
     calc_avg_hw_all = sum(ls_avg_hm_all)/len(ls_avg_hm_all)
-
+    calc_avg_hw_audi = sum(ls_audi)/len(ls_audi)
     # Make the sentence for all vehicles
     w_avg_cm_all = ('Out of all ' + str(len(ls_avg_cm_all))
         + ' vehicles, the average city mpg is ' + str(round(calc_avg_cm_all,2)) + '\n\n')
     w_avg_hm_all = ('Out of all ' + str(len(ls_avg_hm_all))
         + ' vehicles, the average highway MPG is ' + str(round(calc_avg_hw_all,2)) + '\n\n')
 
+    w_avg_hm_audi = ('If we only look at the ' + str(len(ls_audi))
+        + ' audis, the average highway MPG is ' + str(round(calc_avg_hw_audi,2)) + '\n\n')
     #-----PRINT TO TEXT FILE-----------------------------------------------------------------------------#
     # 1 - Create/Open File
     output = open('jsimo62_assignment3.txt', 'w+')
     # 2 - Write to file
     output.write(w_avg_cm_all)
     output.write(w_avg_hm_all)
+    output.write(w_avg_hm_audi)
     output.close()
 
     #-----TEST OUTPUTS ----------------------------------------------------------------------------------#
     # Print sets to command line
-    print(s_manuf)
-    print(s_class)
+    #print(s_manuf)
+    #print(s_class)
     # Pring grouped by list to command newline
     #print(ls_avg_hm_manuf)
     #print(ls_avg_hm_class)
