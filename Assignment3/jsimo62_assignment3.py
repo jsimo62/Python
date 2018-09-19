647ca970f397703e8fe1bcc1bc811e1f651eccd5
jsimo62 <42632473+jsimo62@users.noreply.github.com>
2018-09-19 02:09:01 -0500

    started creating funcitons and calling them

--- Assignment3/jsimo62_assignment3.py
+++ Assignment3/jsimo62_assignment3.py
@@ -1,40 +1,60 @@
 # Import CSV class to work with CSV files
 import csv
 
+# initiate list
+ls_avg_cm_all, ls_avg_hm_all, ls_avg_hm_class, ls_avg_hm_manuf = [], [], [], []
+# initiate sets
+s_manuf, s_class = set(),set()
 
-res_attempt = 0
-while res_attempt < 4:
-    #print(str(res_attempt))
+ls_audi= []
 
-    response = input("Would you like to know the mpg for all vehicles\nin the list or the mpg by group? (all / group) ")
-    if response == "all":
-        print("all")
-        res_attempt = 5
-    elif response == "group":
-        print("group")
-        res_attempt = 5
-    else:
-        res_attempt += 1
-        if res_attempt <= 2:
-            print("\nInvalid Repsonse. Only 'all' or by 'group' are accepted responses.")
-            print(str(res_attempt))
-        elif res_attempt <=3:
-            print("This is your last attempt.  Please type either 'all or 'group' to continue.\n**The program will end after 1 more inncorrect entry**")
-            print(str(res_attempt))
-        else:
-            print("\nGood Bye")
-            print(str(res_attempt))
+def mpg_calc(v_list):
+    # Calculate the average
+    calc = sum(v_list)/len(v_list)
+    return calc
+
+def print_to_file(v_list):
+    #-----PRINT TO TEXT FILE-----------------------------------------------------------------------------#
+    # 1 - Create/Open File
+    output = open('jsimo62_assignment3.txt', 'w+')
+    # 2 - Write to file
+    output.write(w_avg_cm_all)
+    output.write(w_avg_hm_all)
+    output.write(w_avg_hm_audi)
+    output.close()
+    return
+
+# res_attempt = 0
+# while res_attempt < 4:
+#     #print(str(res_attempt))
+#
+#     response = input("Would you like to know the mpg for all vehicles\nin the list or the mpg by group? (all / group) ")
+#     if response == "all":
+#         # Calculate the average for all vehicles
+#         calc_avg_cm_all = mpg_calc(ls_avg_cm_all)
+#         calc_avg_hw_all = mpg_calc(ls_avg_hm_all)
+#         print("all")
+#         res_attempt = 5
+#     elif response == "group":
+#         print("group")
+#         res_attempt = 5
+#     else:
+#         res_attempt += 1
+#         if res_attempt <= 2:
+#             print("\nInvalid Repsonse. Only 'all' or by 'group' are accepted responses.")
+#             print(str(res_attempt))
+#         elif res_attempt <=3:
+#             print("This is your last attempt.  Please type either 'all or 'group' to continue.\n**The program will end after 1 more inncorrect entry**")
+#             print(str(res_attempt))
+#         else:
+#             print("\nGood Bye")
+#             print(str(res_attempt))
+
 
 #-----WORK WITH THE FILE---------------------------------------------------------------------------------#
 with open('mpg.csv', newline='') as csvfile:
     f_mpg = csv.DictReader(csvfile)
 
-    # initiate list
-    ls_avg_cm_all, ls_avg_hm_all, ls_avg_hm_class, ls_avg_hm_manuf = [], [], [], []
-    # initiate sets
-    s_manuf, s_class = set(),set()
-
-    ls_audi= []
     # Loop through csv
     for row in f_mpg:
         # Create a set of unique manufacurers and classes
@@ -67,17 +87,20 @@ with open('mpg.csv', newline='') as csvfile:
     #print(ls_audi)
 
     # Calculate the average for all vehicles
-    calc_avg_cm_all = sum(ls_avg_cm_all)/len(ls_avg_cm_all)
-    calc_avg_hw_all = sum(ls_avg_hm_all)/len(ls_avg_hm_all)
-    calc_avg_hw_audi = sum(ls_audi)/len(ls_audi)
+    calc_avg_cm_all = mpg_calc(ls_avg_cm_all)
+    calc_avg_hw_all = mpg_calc(ls_avg_hm_all)
+    calc_avg_hw_audi = mpg_calc(ls_audi)
+    # calc_avg_cm_all = sum(ls_avg_cm_all)/len(ls_avg_cm_all)
+    # calc_avg_hw_all = sum(ls_avg_hm_all)/len(ls_avg_hm_all)
+    # calc_avg_hw_audi = sum(ls_audi)/len(ls_audi)
     # Make the sentence for all vehicles
     w_avg_cm_all = ('Out of all ' + str(len(ls_avg_cm_all))
-        + ' vehicles, the average city mpg is ' + str(round(calc_avg_cm_all,2)) + '\n\n')
+        + ' vehicles, the average city mpg is ' + str(round(calc_avg_cm_all,2)) + '\n')
     w_avg_hm_all = ('Out of all ' + str(len(ls_avg_hm_all))
-        + ' vehicles, the average highway MPG is ' + str(round(calc_avg_hw_all,2)) + '\n\n')
+        + ' vehicles, the average highway MPG is ' + str(round(calc_avg_hw_all,2)) + '\n')
 
     w_avg_hm_audi = ('If we only look at the ' + str(len(ls_audi))
-        + ' audis, the average highway MPG is ' + str(round(calc_avg_hw_audi,2)) + '\n\n')
+        + ' audis, the average highway MPG is ' + str(round(calc_avg_hw_audi,2)) + '\n')
     #-----PRINT TO TEXT FILE-----------------------------------------------------------------------------#
     # 1 - Create/Open File
     output = open('jsimo62_assignment3.txt', 'w+')
@@ -95,5 +118,6 @@ with open('mpg.csv', newline='') as csvfile:
     #print(ls_avg_hm_manuf)
     #print(ls_avg_hm_class)
     # Print the sentences in command line
-    #print(w_avg_cm_all)
-    #print(w_avg_hm_all)
+    print(w_avg_cm_all)
+    print(w_avg_hm_all)
+    print(w_avg_hm_audi)
